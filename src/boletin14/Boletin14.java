d4e9d1b416d88a9504310bb8b865edfcc8244b0a
David <David@LAPTOP-VSEKM5I2>
2017-12-17 11:25:30 +0000

    COMMIT GITHUB

---
+++ src/boletin14/Boletin14.java
@@ -0,0 +1,38 @@
+
+package boletin14;
+
+/**
+ *
+ * @author David
+ */
+public class Boletin14 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Garaxe coche1=new Garaxe();
+        coche1.calcularPlazas();
+        Garaxe coche2=new Garaxe();
+        coche2.calcularPlazas();
+        Garaxe coche3=new Garaxe();
+        coche3.calcularPlazas();
+        Garaxe coche4=new Garaxe();
+        coche4.calcularPlazas();
+        Garaxe coche5=new Garaxe();
+        coche5.calcularPlazas();
+        Garaxe coche6=new Garaxe();
+        coche6.calcularPlazas();
+        Garaxe coche7=new Garaxe();
+        coche7.calcularPlazas();
+        
+        coche1.mostratFactura();
+        coche2.mostratFactura();
+        coche3.mostratFactura();
+        coche4.mostratFactura();
+        coche5.mostratFactura();
+        coche6.mostratFactura();
+                                                
+    }
+    
+}
